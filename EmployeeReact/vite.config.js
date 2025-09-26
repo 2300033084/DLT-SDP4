3ec4547321744b51df0b768777fceb114b971c41
sanjaysammy2005 <sanjayyy19official@gmail.com>
2025-09-26 12:19:46 +0530

    v1.7

--- EmployeeReact/vite.config.js
+++ EmployeeReact/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Set the base path for your application in the Docker container
-  base: '/',
+  // CHANGE THIS: Use './' (relative path) instead of '/' (absolute path)
+  base: './', // <-- UPDATED LINE
   build: {
     // This setting ensures that the output is compatible with older browsers if needed
     target: 'es2020',
